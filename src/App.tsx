fd56c05125582aa9a1740a02fa89883b2a098233
Zen_Shadowborne <sergiofs19@gmail.com>
2023-04-11 11:21:21 +0200

    Initial update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1
+        className="
+    text-center
+    text-4xl
+    font-bold
+    text-red-500
+    "
+      >
+        Mi portfolio
+      </h1>
+    </div>
+  );
 }
 
 export default App;
