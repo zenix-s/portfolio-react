58c5d58d653078ae26c934d03731fd54caa74bc4
shadowborne <sergiofs19@gmail.com>
2023-04-14 00:48:22 +0200

    Update about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -3,10 +3,10 @@ import mainimg from "../../assets/main.jpg";
 const About = () => {
   return (
     <section className=" flex h-screen w-full flex-col justify-between xl:flex-row">
-      <div className=" flex h-full w-full items-center justify-center xl:w-1/2">
+      <div className=" flex h-2/5 xl:h-full w-full items-center justify-center xl:w-1/2">
         <img src={mainimg} alt="icon" className="h-full w-full object-cover object-bottom grayscale main-image-animation" />
       </div>
-      <div className="flex h-full w-full flex-col items-center justify-around p-4 xl:w-1/2 xl:items-start xl:justify-center main-section-animation">
+      <div className="flex h-3/5 xl:h-full w-full flex-col items-center justify-around p-4 xl:w-1/2 xl:items-start xl:justify-center main-section-animation">
         <h1 className="flex flex-col items-center gap-1 text-6xl uppercase text-slate-200 sm:text-6xl xl:flex-col xl:items-start xl:gap-4 xl:text-8xl 2xl:text-9xl">
           <span>Sergio</span>
           <span>Fernández</span>
