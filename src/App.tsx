4f524b86a5fedde165dbd519e582d650a113c309
shadowborne <sergiofs19@gmail.com>
2023-04-20 23:34:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import Header from "./components/header/header";
 import About from "./components/me/me";
-import Proyectos from "./components/proyectos/proyectos";
-import UpButton from "./components/UpButton";
 import Footer from "./components/footer/footer";
 function App() {
   return (
