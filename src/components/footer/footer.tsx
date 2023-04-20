3b8735dc67d0db0101e1f6a9d23bd12acae39236
shadowborne <sergiofs19@gmail.com>
2023-04-20 11:54:04 +0200

    Update footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
-  return <footer className="">
-    <a href="mailto:setfernet@gmail.com">Hola</a>
-    
-  </footer>;
+  return (
+    <footer className="">
+      <a href="mailto:setfernet@gmail.com">Hola</a>
+    </footer>
+  );
 };
 
 export default Footer;
