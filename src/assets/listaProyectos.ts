df80456773e85074589f3c2810ab6d40a5093a7f
Zen_Shadowborne <sergiofs19@gmail.com>
2023-05-23 17:26:53 +0200

    Update listaProyectos.ts

--- src/assets/listaProyectos.ts
+++ src/assets/listaProyectos.ts
@@ -1,56 +1,74 @@
-import {DiReact, DiJavascript1, DiPhp, } from "react-icons/di";
-import {SiTailwindcss, SiPrisma, SiNextdotjs} from "react-icons/si";
+import { DiReact, DiJavascript1, DiPhp } from "react-icons/di";
+import { SiTailwindcss, SiPrisma, SiNextdotjs } from "react-icons/si";
 
 const ListaProyectos = [
   {
     nombre: "Varbas",
-    descripcion: "Varbas es una plataforma colaborativa de gestión de proyectos diseñada para equipos de trabajo. Con Varbas, los equipos pueden organizar tareas, asignar responsabilidades, realizar seguimiento del progreso y mantener una comunicación efectiva.",
+    descripcion:
+      "Varbas es una plataforma colaborativa de gestión de proyectos diseñada para equipos de trabajo. Con Varbas, los equipos pueden organizar tareas, asignar responsabilidades, realizar seguimiento del progreso y mantener una comunicación efectiva.",
     imagen: "https://picsum.photos/1920/1080",
-    tecnologias: [
-      DiReact,
-      SiTailwindcss,
-      DiJavascript1,
-      SiNextdotjs,
-      SiPrisma,
-    ],
     href: "https://github.com/zenix-s/varbas-app",
-    tecnologiasNombres: [
-      "React",
-      "Tailwindcss",
-      "Typescript",
-      "Nextjs",
+    tecnologias: [
+      {
+        nombre: "React",
+        icono: DiReact,
+      },
+      {
+        nombre: "Tailwindcss",
+        icono: SiTailwindcss,
+      },
+      {
+        nombre: "Typescript",
+        icono: DiJavascript1,
+      },
+      {
+        nombre: "Nextjs",
+        icono: SiNextdotjs,
+      },
+      {
+        nombre: "Prisma",
+        icono: SiPrisma,
+      },
     ],
   },
   {
     nombre: "Proyecto 2",
     descripcion: "Descripcion del proyecto 2",
     imagen: "https://picsum.photos/1920/1080",
-    tecnologias: [
-      DiReact,
-      SiTailwindcss,
-      DiPhp
-    ],
     href: "#",
-    tecnologiasNombres: [
-      "React",
-      "Tailwindcss",
-      "PHP",
+    tecnologias: [
+      {
+        nombre: "React",
+        icono: DiReact,
+      },
+      {
+        nombre: "Tailwindcss",
+        icono: SiTailwindcss,
+      },
+      {
+        nombre: "PHP",
+        icono: DiPhp,
+      },
     ],
   },
   {
     nombre: "Proyecto 3",
     descripcion: "Descripcion del proyecto 3",
     imagen: "https://picsum.photos/1920/1080",
-    tecnologias: [
-      DiReact,
-      SiTailwindcss,
-      DiJavascript1,
-    ],
     href: "#",
-    tecnologiasNombres: [
-      "React",
-      "Tailwindcss",
-      "Javascript",
+    tecnologias: [
+      {
+        nombre: "React",
+        icono: DiReact,
+      },
+      {
+        nombre: "Tailwindcss",
+        icono: SiTailwindcss,
+      },
+      {
+        nombre: "Javascript",
+        icono: DiJavascript1,
+      },
     ],
   },
 ];
